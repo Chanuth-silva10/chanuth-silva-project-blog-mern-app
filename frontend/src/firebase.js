b73591828256068720e47a4bdfba0ff113bb1441
Chanuth-silva10 <chanuthmaduka1986@gmail.com>
2024-08-16 23:53:04 +0530

    implement google OAuth function

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOLspQI4hE1LuRiFOOyPt1OH-KKdmgD3c",
+  authDomain: "my-blog-6703b.firebaseapp.com",
+  projectId: "my-blog-6703b",
+  storageBucket: "my-blog-6703b.appspot.com",
+  messagingSenderId: "135310824618",
+  appId: "1:135310824618:web:f95c6f4b75697c4f1c2cd3"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
